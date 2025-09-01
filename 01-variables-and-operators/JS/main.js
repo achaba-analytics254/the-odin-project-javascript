b457c1d85e9746dfacd6fd082bc7d937a56190fc
achaba-analytics <achaba.analytics@gmail.com.noreply.github.com>
2025-09-02 02:03:13 +0300

    javascript basics

---
+++ 01-variables-and-operators/JS/main.js
@@ -0,0 +1,50 @@
+console.log('Hello, World');
+
+let my_name = 'Achaba'; // let keyword can be reassigned
+let surname = 'Otieno';
+
+console.log(my_name, surname);
+
+let my_age = 30;
+console.log(my_age);
+
+my_age = 40;
+
+console.log(my_age) // prints 40 instead of 30.
+
+const pi = 3.14; // const keyword cannot be reassigned
+console.log(pi);
+
+// pi = 10;
+
+// console.log(pi); // This will result to an error.
+
+// ********** Numbers **************
+
+console.log(30 + 40);
+
+console.log(4 + 6 + 9) / 77;
+
+let a = 10;
+
+console.log(a)
+
+a = 30;
+
+let b = a * 7;
+
+console.log(b);
+
+const max = 57;
+const actual = max - 13;
+const percentage = actual / max;
+console.log(percentage);
+
+const default_time = 60;
+const study_time = 25;
+const rest_time = 15;
+let my_time = study_time + rest_time;
+console.log(my_time)
+
+console.log(default_time - my_time);
+
