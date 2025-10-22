147a25522a8f4075a73a39347b56a824bc469ce7
djangozent <djangozent@gmail.com>
2025-10-23 00:14:37 +0300

    break, continue and do...while

--- 05-loops/JS/main.js
+++ 05-loops/JS/main.js
@@ -41,4 +41,72 @@ for (let i = 0; i < cats.length; i++){
 
 // Exiting loops using the break statement
 
+/* const contacts = [
+    'Ali: 254700000011',
+    'Alice: 254700000022',
+    'Achaba: 254700000033',
+    'Sam: 254700000044',
+    'David: 254700000055',
+    'Alex: 254700000066',
+    'John: 254700000077',
+    'Hannah: 254700000088',
+    'Judy: 254700000099'
+];
 
+const para = document.querySelector("p");
+const input = document.querySelector("input");
+const btn = document.querySelector("button");
+
+btn.addEventListener("click", () => {
+  const searchName = input.value.toLowerCase();
+  input.value = "";
+  input.focus();
+  para.textContent = "";
+  for (const contact of contacts) {
+    const splitContact = contact.split(":");
+    if (splitContact[0].toLowerCase() === searchName) {
+      para.textContent = `${splitContact[0]}'s number is ${splitContact[1]}.`;
+      break;
+    }
+  }
+  if (para.textContent === "") {
+    para.textContent = "Contact not found.";
+  }
+}); */
+
+// Skipping iteration with continue statement
+
+const para = document.querySelector('p');
+const input = document.querySelector('input');
+const btn = document.querySelector('button');
+
+btn.addEventListener('click', () => {
+    para.textContent = 'Output: ';
+    const num = input.value;
+    input.value = '';
+    input.focus();
+    for (let i = 1; i <= num; i++){
+        let sqRoot = Math.sqrt(i);
+        if (Math.floor(sqRoot) !== sqRoot){
+            continue;
+        }
+        para.textContent += `${i}`;
+    }
+});
+
+// while and do....while loops
+
+const cats6 = ['Pete', 'Biggles', 'Jasmine'];
+
+let myFavoriteCats = 'My cats are called: ';
+
+let i = 0;
+
+while (i < cats6.length){
+    if (i ===  cats6.length - 1){
+        myFavoriteCats  += `${cats6[i]}, `;
+    }
+
+    i++;
+}
+console.log(myFavoriteCats);
