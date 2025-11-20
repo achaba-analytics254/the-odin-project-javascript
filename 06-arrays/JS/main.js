85d013d6a09907355164c00f08053be0c5642344
achaba-analytics254 <djangozent@gmail.com>
2025-11-21 00:24:19 +0300

    Array Methods

--- 06-arrays/JS/main.js
+++ 06-arrays/JS/main.js
@@ -127,3 +127,93 @@ console.log(String(toStringArray) === '1,2,3'); // returns true
 console.log([] + 1);
 console.log([1] + 1);
 console.log([1,2] + 1);
+
+
+
+
+//-----------------------Array Methods--------------------------//
+
+// spilce
+
+let arr3 = ['I', 'go', 'home'];
+delete arr3[1]; // removes go
+console.log(arr[1]); // undefined
+console.log(arr3.length); // 3 - the element was removed but the array still has 3 elements
+
+// delete obj.key removes a value by the key.
+// that is where arr3.splice comes in.
+
+let arr4 = ['I', 'study', 'Javascript'];
+arr4.splice(1, 1);
+console.log(arr4);
+
+// in this example we remove 3 eleements and replace with two
+let arr5 = ['I', 'study', 'Javascript', 'right', 'now'];
+arr5.splice(0, 3, 'Let\'s', 'dance');
+console.log(arr5);
+// Not that negative indexes are also allowed
+
+//-----------------slice-----------------------//
+// The method arr.slice is much simpler than the similar-looking arr.splice.
+
+let arr6 = ['t', 'e', 's', 't'];
+console.log(arr6.slice(1, 3));
+console.log(arr6.slice(-2));
+
+//-----------------concat-------------------//
+//The method arr.concat creates a new array that includes values from other arrays 
+// and additional items.
+
+let arr7 = [1, 2];
+console.log(arr7.concat([3, 4])); // create an array from: arr and [3,4] - result = 1234.
+console.log(arr7.concat([3, 4], [5, 6])); // create an array from: arr and [3,4] and [5,6]
+console.log(arr7.concat([3, 4], 5, 6)); // create an array from: arr and [3,4], then add values 5 and 6
+
+//------------------------Iterate: forEach-------------------//
+// The arr.forEach method allows to run a function for every element of the array.
+
+['Achaba', 'John', 'Peter'].forEach(console.log);
+
+['Achaba', 'John', 'Peter'].forEach((item, index, array) => {
+   console.log(`${item} is at index ${index} in ${array}`);
+});
+
+//------------------Searching in array-----------------------//
+
+// indexOf/lastIndexOf and includes
+// The methods arr.indexOf and arr.includes have the similar syntax and do essentially the same as their 
+// string counterparts, but operate on items instead of characters:
+// Usually, these methods are used with only one argument: the item to search. By default, 
+// the search is from the beginning.
+
+let arr8 = [1, 0, false];
+console.log(arr8.indexOf(0));
+console.log(arr8.indexOf(false));
+console.log(arr8.indexOf(null));
+
+console.log(arr8.includes(1));
+
+// The method arr.lastIndexOf is the same as indexOf, but looks for 
+// from right to left.
+
+let ourFruits = ['Apples', 'Mangoes', 'Apples']
+
+//alert(ourFruits.indexOf('Apples'));
+//alert(ourFruits.lastIndexOf('Apples'));
+
+
+
+//-------------------find and findIndex/findLastIndex---------------------//
+
+// Imagine we have an array of objects. How do we find an object with a 
+// specific condition?
+// Here the arr.find(fn) method comes in handy.
+
+let users = [
+    {id: 1, name: 'Achaba'},
+    {id: 1, name: 'John'},
+    {id: 1, name: 'Peter'},
+];
+
+let user = users.find(item => item.id == 1);
+console.log(user.name);
